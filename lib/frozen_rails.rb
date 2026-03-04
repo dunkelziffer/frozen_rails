03c2557ac13c6c35fa97c32a0301b4da9782ec4c
Klaus Weidinger <weidkl@gmx.de>
2026-03-04 19:49:11 +0100

    Delete unnecessary railtie

--- lib/frozen_rails.rb
+++ lib/frozen_rails.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
 require "frozen_rails/version"
-require "frozen_rails/railtie" if defined?(Rails::Railtie)
--- lib/frozen_rails/railtie.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module FrozenRails # :nodoc:
-  class Railtie < ::Rails::Railtie # :nodoc:
-    generators do
-      require_relative "../generators/frozen/md_generator"
-    end
-  end
-end
