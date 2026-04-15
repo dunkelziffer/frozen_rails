79e60b0dae7e1f33675aeb86e449ce0ca2448a03
Klaus Weidinger <weidkl@gmx.de>
2026-04-15 19:31:40 +0200

    Prevent generating controller tests

---
+++ lib/generators/frozen/templates/db/initializers/skip_controller_tests.rb
@@ -0,0 +1,8 @@
+ActiveSupport.on_load(:after_initialize) do
+  require "rails/generators"
+  require "rails/generators/rails/scaffold_controller/scaffold_controller_generator"
+
+  Rails::Generators::ScaffoldControllerGenerator.class_eval do
+    remove_hook_for :test_framework
+  end
+end
